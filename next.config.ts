094ec485d23d1c5c8759abc6b21668b256362c34
yingzi0129 <yingzi0129@gmail.com>
2026-03-07 18:31:57 +0800

    修复CDN不缓存

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,19 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, s-maxage=86400, stale-while-revalidate=86400',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
